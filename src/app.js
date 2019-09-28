2ab9e8e865e73171957e559ca277606f5d408a4d
sarampau <mannysth@yahoo.com>
2019-09-27 23:43:47 -0700

    converts App to hooks and reverts Toggle to test functionality

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Head from './components/head'
 import Input from './components/input';
 import Output from './components/output';
@@ -7,46 +7,23 @@ import Dropdown from './components/dropdown';
 import Toggle from './components/toggle';
 import './css/style.css';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            input: '',
-            output: '',
-            baseNum: ''
-            // toggle: false
-        }
-        this.handleInput = this.handleInput.bind(this);
-        this.handleClick = this.handleClick.bind(this);
-        this.handleBaseNum = this.handleBaseNum.bind(this);
-        // this.handleToggle = this.handleToggle.bind(this);
-    }
+function App() {
 
-    handleInput(e) {
-        this.setState({
-            input: e.target.value
-        });
-    }
+    const [input, setInput] = useState('');
+    const [output, setOutput] = useState('');
+    const [baseNum, setBaseNum] = useState('');
+    const [toggle, setToggle] = useState(false);
 
-    handleBaseNum(e) {
-        this.setState({
-            baseNum: e.target.value
-        })
-    }
+    const handleInput = e => setInput(e.target.value);
 
-    // handleToggle() {
-    //     this.setState({
-    //         toggle: !this.state.toggle
-    //     })
-    // }
+    const handleBaseNum = e => setBaseNum(e.target.value);
 
-    handleClick() {
+    const handleToggle = () => setToggle(!toggle);
+
+    const handleClick = () => {
         let output1 = 0;
         let output2 = '';
         let multiplier = 1;
-        let input = this.state.input;
-        let baseNum = this.state.baseNum;
-        let toggle = this.state.toggle;
             if (toggle === false) {
                 for (let i = input.length - 1; i >= 0; i--) {
                     if (input.length > 15) {
@@ -70,11 +47,9 @@ class App extends Component {
                         multiplier = multiplier * baseNum;
                     }
                 }
-                this.setState({
-                    output: output1
-                })
+                setOutput(output1)
             } 
-            else if (toggle = true){
+            else if (toggle = true) {
                 while ( input > 0 ) { 
                     if (input.length > 15) {
                         alert(`Input is ${input.length} digits long. Please reduce your input to 15 digits or less.`);
@@ -87,41 +62,157 @@ class App extends Component {
                     output2 = ( input % baseNum ) + output2; 
                     input = Math.floor( input / baseNum ); 
                     }
-                this.setState({
-                    output: output2
-                })
+                setOutput(output2);
             }
     }
 
-    render() {
-        return (
-            <div>
-                <Head />
-                <div className='main-container'>
-                    <Toggle 
-                        // handleToggle={this.handleToggle}
-                        // toggle={this.state.toggle}
+    return (
+        <div>
+            <Head />
+            <div className='main-container'>
+                <Toggle 
+                    handleToggle={handleToggle}
+                    toggle={toggle}
+                />
+                <div className='selectors'>
+                    <Input 
+                        handleInput={handleInput}
+                        input={input}    
                     />
-                    <div className='selectors'>
-                        <Input 
-                            handleInput={this.handleInput}
-                            input={this.state.input}    
-                        />
-                        <Dropdown 
-                            baseNum={this.state.baseNum}
-                            handleBaseNum={this.handleBaseNum}
-                        />
-                    </div>
-                    <Button 
-                        handleClick={this.handleClick}
-                    />                   
-                    <Output 
-                        output={this.state.output}
-                    />                    
+                    <Dropdown 
+                        baseNum={baseNum}
+                        handleBaseNum={handleBaseNum}
+                    />
                 </div>
+                <Button 
+                    handleClick={handleClick}
+                />                   
+                <Output 
+                    output={output}
+                />                    
             </div>
-        )
-    }
+        </div>
+    )
 }
+
+// Keep the class component for reference when comparing to hooks component.
+
+// class App extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             input: '',
+//             output: '',
+//             baseNum: ''
+//             // toggle: false
+//         }
+//         this.handleInput = this.handleInput.bind(this);
+//         this.handleClick = this.handleClick.bind(this);
+//         this.handleBaseNum = this.handleBaseNum.bind(this);
+//         // this.handleToggle = this.handleToggle.bind(this);
+//     }
+
+//     handleInput(e) {
+//         this.setState({
+//             input: e.target.value
+//         });
+//     }
+
+//     handleBaseNum(e) {
+//         this.setState({
+//             baseNum: e.target.value
+//         })
+//     }
+
+//     // handleToggle() {
+//     //     this.setState({
+//     //         toggle: !this.state.toggle
+//     //     })
+//     // }
+
+//     handleClick() {
+//         let output1 = 0;
+//         let output2 = '';
+//         let multiplier = 1;
+//         let input = this.state.input;
+//         let baseNum = this.state.baseNum;
+//         let toggle = this.state.toggle;
+//             if (toggle === false) {
+//                 for (let i = input.length - 1; i >= 0; i--) {
+//                     if (input.length > 15) {
+//                         alert(`Input is ${input.length} digits long. Please reduce your input to 15 digits or less.`);
+//                         break;
+//                     }
+//                     if (input < 0) {
+//                         alert('Input must be a positive number.');
+//                         break;
+//                     }
+//                     if (baseNum == '') {
+//                         alert('You must enter a base number.');
+//                         break;
+//                     }
+//                     if (input[i] > baseNum - 1) {
+//                         alert('Input digits must be less than base number.');
+//                         break;
+//                     } 
+//                     else {
+//                         output1 += input[i] * multiplier;
+//                         multiplier = multiplier * baseNum;
+//                     }
+//                 }
+//                 this.setState({
+//                     output: output1
+//                 })
+//             } 
+//             else if (toggle = true){
+//                 while ( input > 0 ) { 
+//                     if (input.length > 15) {
+//                         alert(`Input is ${input.length} digits long. Please reduce your input to 15 digits or less.`);
+//                         break;
+//                     }
+//                     if (input < 0) {
+//                         alert('Input must be a positive number.');
+//                         break;
+//                     }
+//                     output2 = ( input % baseNum ) + output2; 
+//                     input = Math.floor( input / baseNum ); 
+//                     }
+//                 this.setState({
+//                     output: output2
+//                 })
+//             }
+//     }
+
+//     render() {
+//         // console.log(this.props.toggle)
+//         return (
+//             <div>
+//                 <Head />
+//                 <div className='main-container'>
+//                     <Toggle 
+//                         // handleToggle={this.handleToggle}
+//                         // toggle={this.state.toggle}
+//                     />
+//                     <div className='selectors'>
+//                         <Input 
+//                             handleInput={this.handleInput}
+//                             input={this.state.input}    
+//                         />
+//                         <Dropdown 
+//                             baseNum={this.state.baseNum}
+//                             handleBaseNum={this.handleBaseNum}
+//                         />
+//                     </div>
+//                     <Button 
+//                         handleClick={this.handleClick}
+//                     />                   
+//                     <Output 
+//                         output={this.state.output}
+//                     />                    
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
 export default App;
--- src/components/toggle.js
+++ src/components/toggle.js
@@ -1,34 +1,39 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 
-function Toggle() {
-    const [toggle, setToggle] = useState(false);
+// function Toggle() {
+//     const [toggle, setToggle] = useState(false);
+//     const toggleText = () => setToggle(!toggle);
 
-    return (
-        <div className='toggle-container'>
-            <p>{!toggle ? 'Convert to Decimal' : 'Convert from Decimal'}</p>
-            <label className="switch">
-                <input type="checkbox" onClick={() => setToggle(!toggle)}/>
-                <span className="slider round"></span>
-            </label>
-            <p className='instructions'>{!toggle ? 'Enter a value, select a base number and convert the value to a decimal.' : 'Enter a value, select a base number and convert the value to that base system.'}</p>
-        </div>
-    )
-}
+//     console.log(toggle);
 
-// class Toggle extends Component {
-
-//     render() {
-//         return (
-//             <div className='toggle-container'>
-//                 <p>{!this.props.toggle ? 'Convert to Decimal' : 'Convert from Decimal'}</p>
-//                 <label className="switch">
-//                     <input type="checkbox" onClick={this.props.handleToggle}/>
-//                     <span className="slider round"></span>
-//                 </label>
-//                 <p className='instructions'>{!this.props.toggle ? 'Enter a value, select a base number and convert the value to a decimal.' : 'Enter a value, select a base number and convert the value to that base system.'}</p>
-//             </div>
-//         )
-//     }
+//     return (
+//         <div className='toggle-container'>
+//             <p>{!toggle ? 'Convert to Decimal' : 'Convert from Decimal'}</p>
+//             <label className="switch">
+//                 <input type="checkbox" onClick={() => toggleText}/>
+//                 <span className="slider round"></span>
+//             </label>
+//             <p className='instructions'>{!toggle ? 'Enter a value, select a base number and convert the value to a decimal.' : 'Enter a value, select a base number and convert the value to that base system.'}</p>
+//         </div>
+//     )
 // }
+
+
+
+class Toggle extends Component {
+
+    render() {
+        return (
+            <div className='toggle-container'>
+                <p>{!this.props.toggle ? 'Convert to Decimal' : 'Convert from Decimal'}</p>
+                <label className="switch">
+                    <input type="checkbox" onClick={this.props.handleToggle}/>
+                    <span className="slider round"></span>
+                </label>
+                <p className='instructions'>{!this.props.toggle ? 'Enter a value, select a base number and convert the value to a decimal.' : 'Enter a value, select a base number and convert the value to that base system.'}</p>
+            </div>
+        )
+    }
+}
 
 export default Toggle;
